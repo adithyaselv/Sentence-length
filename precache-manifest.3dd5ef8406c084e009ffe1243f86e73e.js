b02688e97509e0d48268a90ca3a67e7caa27458f
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2020-12-02 13:36:42 -0500

    Updates

---
+++ precache-manifest.3dd5ef8406c084e009ffe1243f86e73e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91af01c8ae40be9074aadb52f9e3f26",
+    "url": "/Sentence-length/index.html"
+  },
+  {
+    "revision": "89d5d52d381d9f64532c",
+    "url": "/Sentence-length/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "bf183ced66e04f9f929c",
+    "url": "/Sentence-length/static/css/main.ab830e05.chunk.css"
+  },
+  {
+    "revision": "89d5d52d381d9f64532c",
+    "url": "/Sentence-length/static/js/2.6e966e80.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Sentence-length/static/js/2.6e966e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf183ced66e04f9f929c",
+    "url": "/Sentence-length/static/js/main.fbd7b7fa.chunk.js"
+  },
+  {
+    "revision": "2be32974bfcc6194ce5d",
+    "url": "/Sentence-length/static/js/runtime-main.ea6150c6.js"
+  }
+]);
